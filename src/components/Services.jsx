b76ae6030931a8df55ef7402ea0a37550a8bf5bd
Harkanwal Singh Ji <harkanwal.singh@indiamart.com>
2025-06-21 21:52:39 +0530

    Service page creation

---
+++ src/components/Services.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Services = () => {
+    return (
+        <div className='text-white py-16'>
+            <div className='container mx-auto px-4 text-center'>
+                <h2 className='text-3xl md:text-4xl font-bold mb-8 underline'>Services</h2>
+                <p className='mb-12 text-gray-400 text-center'>
+                    Lorem ipsum paragraph text Lorem ipsum paragraph text Lorem ipsum paragraph text Lorem ipsum paragraph text Lorem ipsum paragraph text Lorem ipsum paragraph text</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Services
